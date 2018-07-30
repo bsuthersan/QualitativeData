a61e90cdda4dd552e4b46f44c5b71058982e83ca
bsuthersan <jenkinsbridget4@gmail.com>
2018-07-30 08:59:28 +0100

    Add files via upload

--- Obama_Sentinent analysis.R
+++ Obama_Sentinent analysis.R
@@ -1,18 +1,35 @@
 library(tm)
 library(tidytext)
 library(tidyverse)
-library(wordcloud2)
-
-setwd("~/Documents/Repositories")
+library(SnowballC)
+library(ggthemes)
 
 data <- VCorpus(DirSource("Obama data"))
 
-obamadata <- tidy(data) %>%
+filterwords <- c("ca","d0","d5","d3","d2","applause", "ve")
+
+tidy(data) %>%
   mutate(Year = gsub(".txt","", id)) %>%
   unnest_tokens(word,text) %>%
   anti_join(stop_words, by = "word") %>%
-  group_by(Year, word) %>%
-  summarise(Total = n())
+  count(word, Year, sort=TRUE) %>%
+  filter(!(word %in% filterwords)) %>%
+  mutate(word = case_when(
+    word=="americans" ~ "american",
+    word=="jobs" ~ "job",
+    TRUE ~ word
+  )) %>%
+  group_by(Year) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, -n)) %>%
+  ggplot(aes(x = word, y= n, fill=Year)) +
+  geom_col(show.legend=FALSE) +
+  facet_wrap(~Year, scales = "free_y") +
+  coord_flip() +
+  theme_fivethirtyeight()
+  
+  
   
   
 
